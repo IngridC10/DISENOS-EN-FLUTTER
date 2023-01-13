7ae8b1f34553bfcf462bdae6fe185302ffb8ceeb
Ingrid Calzada <ingridcalzada@MacBook-Pro-de-Ingrid.local>
2023-01-12 19:44:55 -0500

    Agregando imagene s al proyecto

--- disenios/lib/main.dart
+++ disenios/lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:disenios/screens/basic_design.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Material App',
-        home: Center(
-          child: Text('Hola Mundo'),
-        ));
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'basic_design',
+      routes: {'basic_design': (_) => BasicDesignScreen()},
+    );
   }
 }
---
+++ disenios/lib/screens/basic_design.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BasicDesignScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Image(image: AssetImage('assets/landscape.jpeg'))),
+    );
+  }
+}
