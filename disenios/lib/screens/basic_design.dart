e8f7739e943a7178f955ba5d26a6abdc1bc73c93
Ingrid Calzada <ingridcalzada@MacBook-Pro-de-Ingrid.local>
2023-01-12 23:12:57 -0500

    agregando texto

--- disenios/lib/screens/basic_design.dart
+++ disenios/lib/screens/basic_design.dart
@@ -4,7 +4,35 @@ class BasicDesignScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Image(image: AssetImage('assets/landscape.jpeg'))),
+        body: Column(children: [
+      //Imagen
+      Image(image: AssetImage('assets/landscape.jpeg')),
+
+      //Titulo
+      Title()
+    ]));
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          children: [
+            Text('Oeschinen lake Campoground',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            Text('Kandersteg,Switzerland'),
+          ],
+        ),
+        Icon(Icons.star, color: Colors.red),
+        Text('41'),
+      ],
     );
   }
 }
